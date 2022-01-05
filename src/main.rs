a204200b16cefb668ab1601b88b4bd1a5efcd0ec
tamaron <tamaron1203@gmail.com>
2022-01-05 21:00:28 +0900

    move files

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use mini_redis::{client, Result};
+use mini_redis::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
-
-    client.set("hello", "world".into()).await?;
-
-    let result = client.get("hello").await?;
-
-    println!("got value from the server; result={:?}", result);
     Ok(())
 }
